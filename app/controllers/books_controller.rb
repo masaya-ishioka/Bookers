76cca5e2e606385b892870d4de954068f20421d0
masaya-ishioka <penpenmono31@gmail.com>
2018-11-03 06:20:54 +0000

    [Finish]5-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+
+  def index
+  	@books = Book.all.order(created_at: :desc)
+    @book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  	   flash[:notice]="successfully create!"
+  	   redirect_to book_path(@book.id)
+    else
+      @books = Book.all.order(created_at: :desc)
+       render :index
+    end
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  		flash[:notice]="successfully edit!"
+  		redirect_to book_path(book.id)
+  end
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	flash[:notice]="successfully delete!"
+  	redirect_to books_path
+  end
+
+  def top
+  end
+
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
